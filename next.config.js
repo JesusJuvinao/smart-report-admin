7c006da6d60fbdd7b3c40a1431b58a888510547a
JesusJuvinao <91159962+JesusJuvinao@users.noreply.github.com>
2021-11-25 15:41:09 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
+  future: {
+    webpack5: true // by default, if you customize webpack config, they switch back to version 4. 
+    // Looks like backward compatibility approach.
+  },
+  webpack(config) {
+    config.resolve.fallback = { fs: false }
+    return config
+  }
 }
